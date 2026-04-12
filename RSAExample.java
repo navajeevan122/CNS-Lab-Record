b1af49c3b3412382433c17a06501a1a9177f2ed3
Nava Jeevan Kumar Chitikena <navajeevan122@gmail.com>
2026-04-12 09:21:00 +0530

    Add files via upload

---
+++ RSAExample.java
@@ -0,0 +1,36 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class RSAExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Prime number p: ");
+        BigInteger p = sc.nextBigInteger();
+
+        System.out.print("Enter Prime number q: ");
+        BigInteger q = sc.nextBigInteger();
+
+        BigInteger n = p.multiply(q);
+        BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+
+        System.out.print("Enter public Key e: ");
+        BigInteger e = sc.nextBigInteger();
+
+        BigInteger d = e.modInverse(phi);
+
+        System.out.println("\nPublic Key (e,n): (" + e + "," + n + ")");
+        System.out.println("Private Key (d,n): (" + d + "," + n + ")");
+
+        System.out.print("\nEnter message (number): ");
+        BigInteger message = sc.nextBigInteger();
+
+        BigInteger cipher = message.modPow(e, n);
+        System.out.println("Encrypted message: " + cipher);
+
+        BigInteger decrypted = cipher.modPow(d, n);
+        System.out.println("Decrypted message: " + decrypted);
+
+        sc.close();
+    }
+}
